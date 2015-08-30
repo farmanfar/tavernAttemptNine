6e616a1234c6277042948c8948408ab7535d4e3b
Farman <bigtomcurbstomp@gmail.com>
2015-08-30 19:40:45 -0400

    init

---
+++ GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  tavernAttemptNine
+//
+//  Created by Farman on 8/30/15.
+//  Copyright (c) 2015 Thomas Guererri. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+      backgroundColor = UIColor.blueColor()
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+ 
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  tavernAttemptNine
+//
+//  Created by Farman on 8/30/15.
+//  Copyright (c) 2015 Thomas Guererri. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    var scene: GameScene!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //configure the view
+        
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        //present the scene
+        skView.presentScene(scene)
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
